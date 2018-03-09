0af8ae4a78c8816f8ecc10201e4b6bc2c59bc777
Sai Bharath Uppinakuduru <bharathuaithal@gmail.com>
2018-03-09 19:24:58 +0000

    revision 2 CodeX-Upload

---
+++ src/codex/Bootstrap.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codex;
+
+/**
+ *
+ * @author Sai
+ */
+public class Bootstrap {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static String textColour = "#abacad";
+
+    private static String backgroundSlide = "#3B3F42";
+
+    private static String error = "#ff0000";
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    }
+
+    public static String getTextColour() {
+        return textColour;
+    }
+
+    public static String getBackgroundSlide() {
+        return backgroundSlide;
+    }
+
+    public static String getError() {
+        return error;
+    }
+}
